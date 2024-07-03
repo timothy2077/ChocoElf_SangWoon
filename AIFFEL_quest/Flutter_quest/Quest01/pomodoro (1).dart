cc231dae1c2c2bb708d448e285a2dcd8cdba164a
chocoelf <168162744+chocoelf@users.noreply.github.com>
2024-07-03 09:27:13 +0900

    Update pomodoro (1).dart

--- AIFFEL_quest/Flutter_quest/Quest01/pomodoro (1).dart
+++ AIFFEL_quest/Flutter_quest/Quest01/pomodoro (1).dart
@@ -1,11 +1,14 @@
+
 import 'dart:async';
 
 class Pomodoro {
-  int count = 0;
+  int count = 1;
   late int work_minutes;
   late int rest_minutes_1;
   late int rest_minutes_2;
-  bool check = false;
+  int seconds = 60;
+  int minutes = 0;
+  
   Pomodoro(this.work_minutes, this.rest_minutes_1, this.rest_minutes_2);
 
   // 뽀모도로 타이머 시작 메소드
@@ -33,39 +36,19 @@ class Pomodoro {
       }
     }
     */
-
-    count += 1;
     print("$count회차 작업을 시작하겠습니다. ");
 
-    if (count % 4 == 0) {
-      if (check == false){
-        timer('w');
-      }else {
-        timer('r2');
-      }
-    }else if (count % 4 != 0){
-      if (check == false){
-        timer('w');
-      }else {
-        timer('r1');
-      }
-    }
 
     // stop();
   }
 
   // 타이머 메소드 (매개변수 : 워크/휴식1/휴식2)
-  void timer(String mode) {
-    int seconds = 60;
-    int minutes = 0;
+  void timer1() {
 
-    if (mode == 'w') {
-      minutes = work_minutes - 1;
-    } else if (mode == 'r1') {
-      minutes = rest_minutes_1 - 1;
-    } else if (mode == 'r2') {
-      minutes = rest_minutes_2 - 1;
-    }
+
+    count += 1;
+    minutes = work_minutes - 1;
+
 
     // 1초 간격으로 반복 실행되는 타이머 생성
     Timer.periodic(Duration(seconds: 1), (timer) {
@@ -75,15 +58,66 @@ class Pomodoro {
         seconds = 59;
         if (minutes <= 0) {
           timer.cancel();
+          print('작업 시간이 종료되었습니다.');
+          if (count % 4 == 0){
+            print("$count회차 휴식을 시작하겠습니다. ");
+            timer3();
+          }else{
+            print("$count회차 휴식을 시작하겠습니다. ");
+            timer2();
+          }         
+        } else {
+          minutes -= 1;
+          print('$minutes:$seconds');
+        }
+      } else {
+        seconds -= 1;
+        print('$minutes:$seconds');
+      }
+    });
+  }
+  
+  void timer2(){
+    
+    minutes = rest_minutes_1;
 
-          if (mode == 'w') {
-            print('작업 시간이 종료되었습니다.');
-            check = true;
-          } else if (mode == 'r1') {
-            print('$rest_minutes_1분 휴식 시간이 종료되었습니다.');
-          } else if (mode == 'r2') {
-            print('$rest_minutes_2분 휴식 시간이 종료되었습니다.');
-          }
+
+    // 1초 간격으로 반복 실행되는 타이머 생성
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      // print('Tick: ${timer.tick}'); // timer.tick은 타이머가 몇 번 실행되었는지 알려줍니다.
+
+      if (seconds <= 0) {
+        seconds = 59;
+        if (minutes <= 0) {
+          timer.cancel();
+          print('$rest_minutes_1분 휴식 시간이 종료되었습니다.');
+          timer1();
+        } else {
+          minutes -= 1;
+          print('$minutes:$seconds');
+        }
+      } else {
+        seconds -= 1;
+        print('$minutes:$seconds');
+      }
+    });
+  }
+  
+  void timer3(){
+    
+    minutes = rest_minutes_2 - 1;
+
+
+    // 1초 간격으로 반복 실행되는 타이머 생성
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      // print('Tick: ${timer.tick}'); // timer.tick은 타이머가 몇 번 실행되었는지 알려줍니다.
+
+      if (seconds <= 0) {
+        seconds = 59;
+        if (minutes <= 0) {
+          timer.cancel();
+          print('$rest_minutes_2분 휴식 시간이 종료되었습니다.');
+          timer1();
         } else {
           minutes -= 1;
           print('$minutes:$seconds');
@@ -144,6 +178,6 @@ void main() {
 일단 와일문 적용이 힘들어서 중간에 시간이 모자랐습니다. ㅠㅠ
 
 - 김주현 :
- 반복문과 비동기 처리에 대한 부분이 혼동이 되어서 아직 최종 완료가 되지 못한 것이 아쉽습니다. 
+ 반복문과 비동기 처리에 대한 부분이 혼동이 되어서 아직 최종 완료가 되지 못한 것이 아쉽습니다.
 
 */
